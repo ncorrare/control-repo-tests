5fc9bdeb3b26481df980125159bf2b42d6a20ad2
Nicolas Corrarello <ncorrare@gmail.com>
2016-12-02 13:34:49 +0000

    Removing hardcoded SHA

--- scripts/code_manager_config_version.rb
+++ scripts/code_manager_config_version.rb
@@ -7,7 +7,7 @@ r10k_deploy_file_path = File.join(environmentpath, environment, '.r10k-deploy.js
 
 sha = JSON.parse(File.read(r10k_deploy_file_path))['signature']
 
-str  = `git ls-remote --tags https://github.com/ncorrare/control-repo-tests | grep 5e383672601b262e5aef4f01845297dbc5915a03 |awk -F/ '{print $3}'`
+str  = `git ls-remote --tags https://github.com/ncorrare/control-repo-tests | grep #{environment} |awk -F/ '{print $3}'`
 tag = str.delete!("\n")
 if str.nil?
 		output = "#{environment}/#{sha}"
