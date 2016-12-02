1f4d0aaea5c9c4fe663840b97c420e3d21734ea9
Nicolas Corrarello <ncorrare@gmail.com>
2016-12-02 13:26:28 +0000

    Refactoring to make it way easier

--- scripts/code_manager_config_version.rb
+++ scripts/code_manager_config_version.rb
@@ -1,5 +1,4 @@
 require 'json'
-require 'rugged'
 
 environmentpath = ARGV[0]
 environment     = ARGV[1]
@@ -8,14 +7,13 @@ r10k_deploy_file_path = File.join(environmentpath, environment, '.r10k-deploy.js
 
 sha = JSON.parse(File.read(r10k_deploy_file_path))['signature']
 
-repo = Rugged::Repository.new('https://github.com/ncorrare/control-repo-tests.git')
-tag  = repo.lookup(sha)
-str  = tag.name
+str  = `git ls-remote --tags https://github.com/ncorrare/control-repo-tests | grep 5e383672601b262e5aef4f01845297dbc5915a03 |awk -F/ '{print $3}'`
+tag = str.delete!("\n")
 if str.nil?
-	output = "#{environment}/#{sha}"
+		output = "#{environment}/#{sha}"
 else
-	
-	output = "#{environment}/#{str}"
+
+		output = "#{environment}/#{tag}"
 end
 #output the sha1 from the control-repo
 puts output
